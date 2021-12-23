34978c08fb0a2a0997914e027a3ee876b7e9663d
shrimanwar92 <shrimanwar92@gmail.com>
2021-12-23 13:15:35 +0530

    added nation endpoint

--- server.js
+++ server.js
@@ -11,8 +11,15 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-
   axios.get('https://random-data-api.com/api/users/random_user').then(function (response) {
+      res.json(response.data);
+  }).catch(function (error) {
+      console.log(error);
+  });
+});
+
+app.get('/nation', (req, res) => {
+  axios.get('https://random-data-api.com/api/nation/random_nation').then(function (response) {
       res.json(response.data);
   }).catch(function (error) {
       console.log(error);
